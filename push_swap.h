03935d7dd7ce846bdbaf240f7780f8160c76f5fb
Patricia Lopez Loro <plopez-l@c1r11s6.42madrid.com>
2026-07-13 12:21:20 +0200

    prueba h

---
+++ push_swap.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plopez-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/13 11:27:45 by plopez-l          #+#    #+#             */
+/*   Updated: 2026/07/13 11:31:55 by plopez-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+// comprobar que librerías son necesarias.
+
+typedef struct s_list
+{
+	int				*content;
+	struct s_list	*next;
+	struct s_list	*prev;
+}	t_list;
+
+
+//meter prototipo de las funciones.
+
+
+#endif
