81a8375110cd967d817489911be0b94fab9f5174
Patruskiss <patricialorolopez@hotmail.com>
2026-07-14 14:52:59 +0200

    modificaciones

--- push_swap.c
+++ push_swap.c
@@ -52,13 +52,48 @@ int	str_to_checked_int(const char *str)
 	return ((int)(num * sign));
 }
 
+int	has_duplicates(int *numbers, int count)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < count)
+	{
+		j = i + 1;
+		while (j < count)
+		{
+			if (numbers[i] == numbers[j])
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	int	i;
 	int	j;
+	int	count;
+	int	max_size;
 	char	**nbr_strs;
-	long	num;
+	int	*numbers;
 
+	if (argc < 2)
+		return (0);
+	max_size = 0;
+	i = 1;
+	while (i < argc)
+	{
+		max_size = max_size + ft_strlen(argv[i]);
+		i++;
+	}
+	numbers = malloc(sizeof(int) * max_size);
+	if (!numbers)
+		return ();
+	count = 0;
 	i = 1;
 	while (i < argc)
 	{
@@ -68,7 +103,8 @@ int	main(int argc, char **argv)
 		{
 			if (!is_valid_number(nbr_strs[j])
 				error_exit();
-			num = str_to_checked_int(nbr_strs[j]);
+			numbers[count] = str_to_checked_int(nbr_strs[j]);
+			count++;
 			j++;
 		}
 		j = 0;
@@ -77,5 +113,7 @@ int	main(int argc, char **argv)
 		free(nbr_strs);
 		i++;
 	}
+	if (has_duplicates(numbers, count))
+		error_exit();
 	return (0);
 }
