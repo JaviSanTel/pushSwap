9976f6c61d31f29033619e507f38972a84c9d8fd
Patricia Lopez Loro <plopez-l@c1r11s3.42madrid.com>
2026-07-16 14:16:15 +0200

    struct para flags

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: plopez-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 11:27:45 by plopez-l          #+#    #+#             */
-/*   Updated: 2026/07/13 11:31:55 by plopez-l         ###   ########.fr       */
+/*   Updated: 2026/07/16 14:12:16 by plopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,16 @@ typedef struct s_list
 	struct s_list	*next;
 	struct s_list	*prev;
 }	t_list;
+
+typedef struct s_flags
+{
+	int	simple = 0;
+	int	medium = 0;
+	int complex = 0;
+	int	adaptive = 0;
+	int	bench = 0;
+}	t_flags;
+
 
 
 //meter prototipo de las funciones.
